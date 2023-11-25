a722ab2db241849d2738b9174cf0c0471cc9da7f
Rajan kumar <krcpr007@gmail.com>
2023-11-26 01:25:43 +0530

    Component Imported

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from './components/Header';
+import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+import data from './Data.json'
+import { useState } from 'react';
+import Filter from './components/Filter';
+import Table from './components/Table';
 function App() {
+  const [rowData, setRowData] = useState(data.response);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Filter setRowData={setRowData} />
+      <Table rowData={rowData} />
+    </>
   );
 }
 
